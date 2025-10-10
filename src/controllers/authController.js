7aaca1aa2eb024a0760a933127f3966005d57b37
PrakashGatway <abroadgateway370@gmail.com>
2025-10-10 13:11:14 +0530

    dd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,21 +35,26 @@ export const sendOtp = async (req, res) => {
 
     await Otp.create({ email, otp });
 
-    await axios.post(
-      "https://api.brevo.com/v3/smtp/email",
-      {
-        sender: { name: "Gateway Abroad", email: "abroadgateway370@gmail.com" },
-        to: [{ email: email, name: "Recipient" }],
-        subject: "Otp For login",
-        htmlContent: `Your OTP is ${otp}. It will expire in 5 minutes.`,
-      },
-      {
-        headers: {
-          "api-key": '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',
-          "Content-Type": "application/json",
+    try {
+      await axios.post(
+        "https://api.brevo.com/v3/smtp/email",
+        {
+          sender: { name: "Gateway Abroad", email: "abroadgateway370@gmail.com" },
+          to: [{ email: email, name: "Recipient" }],
+          subject: "Otp For login",
+          htmlContent: `Your OTP is ${otp}. It will expire in 5 minutes.`,
         },
-      }
-    );
+        {
+          headers: {
+            "api-key": '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',
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error)
+    }
+
     // await sendEmail({
     //   email,
     //   subject: "OTP for Login",
