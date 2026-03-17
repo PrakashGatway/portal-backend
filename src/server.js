92364933bd9e41ff11238c3d3a7c26aa9fc135ce
PrakashGatway <abroadgateway370@gmail.com>
2026-03-17 16:08:33 +0530

    dd

--- src/server.js
+++ src/server.js
@@ -69,7 +69,7 @@ app.use("/uploads", express.static("uploads"));
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://dashboard.gatewayabroadeducations.com",
     methods: ["GET", "POST"]
   },
   credentials: true
